ac8bb9696fa1c3260c6e033b17ecb7ebdce5b317
liamjosephnolan <153128186+liamjosephnolan@users.noreply.github.com>
2023-12-13 10:35:29 +0100

    Added center calc and disply

--- BallTracker.py
+++ BallTracker.py
@@ -1,42 +1,69 @@
+#import libs
 import cv2 as cv
 import numpy as np
 
 # Create video element
 video = cv.VideoCapture(0)
 
-ColorLower = (2, 40, 38)
-ColorUpper = (11, 199, 255)
+#assign upper and lower values from ColorThresholdTesting
+ColorLower = (12 ,70 ,49)
+ColorUpper = (27, 255, 255)
 
-#Capture frame from video and display it on screen
+#Capture frame from webcam, perfom processing and display results
 while(True):
+    #Get webcam frame
     ret, frame = video.read()
 
+    # convert frame to HSV
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
-	# construct a mask for the specified color, then perform
-	# a series of dilations and erosions to remove any small
-	# blobs left in the mask
-
+	# construct a mask for the specified color
     mask = cv.inRange(hsv, ColorLower, ColorUpper)
-    mask = cv.erode(mask, None, iterations=2)
-    mask = cv.dilate(mask, None, iterations=2)
+    # Erode small contours
+    mask = cv.erode(mask, None, iterations=5)
+    # Dilate, idk what this does tbh
+    mask = cv.dilate(mask, None, iterations=5)
+
+    # Find contours on the mask
     cnts, _ = cv.findContours(mask.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    #This is just for debugging
     print("Number of Contours found = " + str(len(cnts))) 
+
+    #See if contours are in frame
+    try:
+        #Iterate through countour and find center (average value)
+        for c in cnts:
+            M = cv.moments(c)
+            cX= int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            
+            
+        # create a blank image to draw contours on
+        contour_img = np.zeros_like(frame)
+
+        # draw contours on the blank image
+        cv.drawContours(contour_img, cnts, -1, (0, 255, 0), 3)
+
+        # add the contours to the original frame
+        result = cv.add(frame, contour_img)
+
+        #Draw a circle on center of contour and label it
+        cv.circle(contour_img, (cX, cY), 7, (255, 255, 255), -1)
+        cv.putText(contour_img, "center", (cX - 20, cY - 20),
+        cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+    # No contour found case
+    except:
+        pass
+
     
-    # create a blank image to draw contours on
-    contour_img = np.zeros_like(frame)
-
-    # draw contours on the blank image
-    cv.drawContours(contour_img, cnts, -1, (0, 255, 0), 3)
-
-    # add the contours to the original frame
-    result = cv.add(frame, contour_img)
-
-
+    # Show original image
     # cv.imshow('frame', result)
 
     # Display the mask if needed
     cv.imshow('mask', mask)
+    # Disp contours
     cv.imshow('Conts', contour_img)
 
 
